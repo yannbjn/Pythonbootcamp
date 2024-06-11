7121ee55e8acf4138ccb7c37fa73e520d48cfd6c
Yann Bejani <yabejani@paul-f5Br1s3.clusters.42paris.fr>
2024-06-11 15:55:09 +0200

    doc

--- Module01/ex01/game.py
+++ Module01/ex01/game.py
@@ -6,7 +6,7 @@
 #    By: yabejani <yabejani@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/11 15:12:08 by yabejani          #+#    #+#              #
-#    Updated: 2024/06/11 15:45:41 by yabejani         ###   ########.fr        #
+#    Updated: 2024/06/11 15:53:07 by yabejani         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,13 +16,14 @@ class GotCharacter:
         self.is_alive = is_alive
 
 class Stark(GotCharacter):
-    def __init__(self, first_name=None, is_alive=True):
-        super().__init__(first_name=first_name, is_alive=is_alive)
-        self.family_name = "Stark"
-        self.house_words = "Winter is Coming"
+	"""A class representing the Stark family. Or when bad things happen to good people."""
+	def __init__(self, first_name=None, is_alive=True):
+		super().__init__(first_name=first_name, is_alive=is_alive)
+		self.family_name = "Stark"
+		self.house_words = "Winter is Coming"
 
-    def print_house_words(self):
-        print(f"House words of {self.first_name} = {self.house_words}")
+	def print_house_words(self):
+		print(f"House words of {self.first_name} = {self.house_words}")
 
-    def die(self):
-        self.is_alive = False
+	def die(self):
+		self.is_alive = False
